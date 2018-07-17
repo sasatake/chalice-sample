81b6fb51ef35ac311246850153f2b4be88485ab1
Akira Satake <Satake-A2@mail.dnp.co.jp>
2018-07-17 18:30:47 +0900

    chalicelibの作成

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from chalice import Chalice
+from chalicelib.services.top import main
 
 app = Chalice(app_name='chalice-sample')
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return main()
